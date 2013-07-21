0366529006682de311e094c0cc15c65d7f999603
Kristian Sons <kristian.sons@dfki.de>
2013-07-21 18:53:48 +0200

    Basic AST transformation to prepare GLSL code generation
    Supports transformation of Math methods and constants and % operator

---
+++ src/generate/glsl/registry/math.js
@@ -0,0 +1,54 @@
+(function(ns){
+
+   var Shade = require("../../../interfaces.js").Shade;
+   var Syntax = require('estraverse').Syntax;
+
+    var MathConstants = ["E", "PI", "LN2", "LOG2E", "LOG10E", "PI", "SQRT1_2", "SQRT2"];
+
+    var removeMemberFromExpression = function(node) {
+        return {
+            type: Syntax.Identifier,
+            name: node.property.name
+        }
+    }
+
+
+
+    var MathEntry  = {
+        abs: removeMemberFromExpression,
+        acos: removeMemberFromExpression,
+        asin: removeMemberFromExpression,
+        atan: removeMemberFromExpression,
+        atan2: function() { return { type: Syntax.Identifier, name: "atan" } },
+        ceil: removeMemberFromExpression,
+        cos:  removeMemberFromExpression,
+        exp: removeMemberFromExpression,
+        floor: removeMemberFromExpression,
+        // imul: removeMemberFromExpression,
+        log: removeMemberFromExpression,
+        max: removeMemberFromExpression,
+        min: removeMemberFromExpression,
+        pow: removeMemberFromExpression,
+        // random: function random() { [native code] }
+        round: removeMemberFromExpression, // Since GLSL 1.3, what does WebGL use?
+        sin:  removeMemberFromExpression,
+        sqrt: removeMemberFromExpression,
+        tan: removeMemberFromExpression,
+        E: function() { return { type: Syntax.Literal, value: Math.E }},
+        PI: function() { return { type: Syntax.Literal, value: Math.PI }}
+    };
+
+    MathConstants.forEach( function(constant) {
+        MathEntry[constant] = function() {
+            return  { type: Syntax.Literal, value: Math[constant], extra: { type: Shade.TYPES.NUMBER } };
+        }
+    });
+
+    var register = function(to) {
+        to["Math"] = MathEntry;
+    };
+
+    ns.MathEntry = MathEntry;
+    ns.register = register;
+
+}(exports));
--- src/generate/glsl/transform.js
+++ src/generate/glsl/transform.js
@@ -1,6 +1,18 @@
 (function (ns) {
 
-    var Base = require("../../base/index.js").Base;
+    var Base = require("../../base/index.js").Base,
+        Shade = require("../../interfaces.js").Shade;
+
+    var ObjectRegistry = {};
+
+    require("./registry/math.js").register(ObjectRegistry);
+
+    var walk = require('estraverse');
+    var Syntax = walk.Syntax;
+
+
+
+
 
     /**
      * Transforms the JS AST to an AST representation convenient
@@ -13,10 +25,69 @@
 
     Base.extend(GLASTTransformer.prototype, {
         transformAAST: function (aast) {
+
+            walk.replace(aast, {
+
+                enter: function(node, parent, cb) {
+                    //console.log("Enter:", node.type);
+                    switch(node.type) {
+                        case Syntax.MemberExpression:
+                            return handleMemberExpression(node, parent, cb);
+                        case Syntax.BinaryExpression:
+                            return handleBinaryExpression(node, parent, cb);
+
+                    }
+                }
+
+
+            });
             return aast;
         }
-    })
+    });
 
+
+
+
+    var handleMemberExpression = function(memberExpression, parent, cb) {
+       // console.log("Member:", memberExpression.object);
+       var object = memberExpression.object,
+           property = memberExpression.property;
+
+
+       if (object.name in ObjectRegistry) {
+            var objectEntry = ObjectRegistry[object.name];
+           if (property.name in objectEntry) {
+               var propertyHandler = objectEntry[property.name];
+               if (typeof propertyHandler == "function") {
+                   return propertyHandler(memberExpression, parent, cb);
+               }
+           }
+       }
+    };
+
+    var handleBinaryExpression = function(binaryExpression, parent, cb) {
+        if (binaryExpression.operator = "%") {
+            return handleModulo(binaryExpression);
+        }
+    }
+
+    var handleModulo = function(binaryExpression) {
+        return {
+            type: Syntax.CallExpression,
+            callee: {
+                type: Syntax.Identifier,
+                name: "mod"
+            },
+            arguments: [
+                binaryExpression.left,
+                binaryExpression.right // TODO: Needs to be number type
+            ]
+        }
+    }
+
+
+
+    // Exports
     ns.GLASTTransformer = GLASTTransformer;
 
 
