d7b29d8fc822c514c559ddc96d12f8825d966f98
Felix Klein <lachsen@cg.uni-saarland.de>
2013-08-14 13:25:35 +0200

    Adapted syntax of nightvision example.

--- data/js/shader/nightvision.js
+++ data/js/shader/nightvision.js
@@ -4,7 +4,7 @@ function hash(n) {
 }
 
 function shade(env) {
-    var p = this.normalizedCoords.xy().times(2).minus(1);
+    var p = this.normalizedCoords.xy().mul(2).sub(1);
 
     var n = p.x(this.width / this.height);
 
@@ -15,12 +15,12 @@ function shade(env) {
     var c = env.channel0.sample2D(this.normalizedCoords);
 
     // flicker, grain, vignette, fade in
-    c = c.plus(Math.sin(hash(env.time)) * 0.01);
-    c = c.plus(hash((hash(n.x()) + n.y()) * env.time) * 0.4);
-    c = c.times(Shade.smoothstep(n.length() * 0.18, 0.8, 0.4));
-    c = c.times(1.9);
-    c = c.times(Shade.smoothstep(0.001, 3.5, env.time));
+    c = c.add(Math.sin(hash(env.time)) * 0.01);
+    c = c.add(hash((hash(n.x()) + n.y()) * env.time) * 0.4);
+    c = c.mul(Shade.smoothstep(n.length() * 0.18, 0.8, 0.4));
+    c = c.mul(1.9);
+    c = c.mul(Shade.smoothstep(0.001, 3.5, env.time));
 
     var s = c.dot(0.2126, 0.7152, 0.0722);
-    return new Vec3(0.2, 1.5 - hash(env.time) * 0.1, 0.4).times(s);
+    return new Vec3(0.2, 1.5 - hash(env.time) * 0.1, 0.4).mul(s);
 }
