86ff4badfcf3dcd5c37ee187586eac164ef18e19
Kristian Sons <kristian.sons@dfki.de>
2013-08-27 10:18:18 +0200

    Examples: Bugfixes and imrpovments in nightvision.js

--- data/js/shader/nightvision.js
+++ data/js/shader/nightvision.js
@@ -4,23 +4,24 @@ function hash(n) {
 }
 
 function shade(env) {
+    var time = env.time || 10.0;
     var p = this.normalizedCoords.xy().mul(2).sub(1);
 
-    var n = p.x(this.width / this.height);
+    var n = p.mul(this.width / this.height, 1.0);
 
     if (Math.abs(p.y()) > .8 || Math.floor(this.coords.y()) % 3.0 > 0.0) {
         return new Vec3(0);
     }
 
-    var c = env.channel0 ? env.channel0.sample2D(this.normalizedCoords).xyz() : new Vec3(0);
+    var c = env.channel0 ? env.channel0.sample2D(this.normalizedCoords).xyz() : new Vec3(0,1,0);
 
     // flicker, grain, vignette, fade in
-    c = c.add(Math.sin(hash(env.time)) * 0.01);
-    c = c.add(hash((hash(n.x()) + n.y()) * env.time) * 0.4);
-    c = c.mul(Shade.smoothstep(n.length() * 0.18, 0.8, 0.4));
+    c = c.add(Math.sin(hash(time)) * 0.01);
+    c = c.add(hash((hash(n.x()) + n.y()) * time) * 0.4);
+    c = c.mul(Shade.smoothstep(n.mul(0.18).length(), 0.8, 0.4));
     c = c.mul(1.9);
-    c = c.mul(Shade.smoothstep(0.001, 3.5, env.time));
+    c = c.mul(Shade.smoothstep(0.001, 3.5, time));
 
     var s = c.dot(0.2126, 0.7152, 0.0722);
-    return new Vec3(0.2, 1.5 - hash(env.time) * 0.1, 0.4).mul(s);
+    return new Vec3(0.2, 1.5 - hash(time) * 0.1, 0.4).mul(s);
 }
