3fcfb129100e1fc827bda8d831ed617f8061eda4
Dmitri Rubinstein <rubinstein@cs.uni-saarland.de>
2013-09-19 12:55:21 +0200

    Fixed simple.js shader.

--- data/js/shader/simple.js
+++ data/js/shader/simple.js
@@ -2,7 +2,7 @@ function shade(env) {
 
     var diffuseColor = new Vec3(1,0,0);
     if(env.diffuseTexture) {
-        diffuseColor.mul(env.diffuseTexture.sample(env.texcoord));
+        diffuseColor.mul(env.diffuseTexture.sample2D(env.texcoord));
     }
     return new Shade().diffuse(diffuseColor, env.normal);
 
