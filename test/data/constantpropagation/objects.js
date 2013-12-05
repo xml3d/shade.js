7ee6cecbf66fe738bcaa8aae57462681afaeed74
Kristian Sons <kristian.sons@dfki.de>
2013-12-05 14:58:12 +0100

    Added two constant propgation tests

---
+++ test/data/constantpropagation/objects.js
@@ -0,0 +1,13 @@
+// Basic Propagation of object values
+/*
+var a = new Vec3(1);
+var a2 = new Vec3(1);
+var b = 1;
+a = new Vec3(1, 2, 3);
+var c = new Vec3(0, 0, 0);
+*/
+var a = new Vec3(1);
+var a2 = a;
+var b = a.x();
+a = new Vec3(1,2,3);
+var c = a.zyx();
