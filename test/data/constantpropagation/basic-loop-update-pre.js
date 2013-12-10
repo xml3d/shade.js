d34503b3d6474522f3228d746da42a7ccad13fc3
Kristian Sons <kristian.sons@dfki.de>
2013-12-10 13:31:17 +0100

    More propagation tests

---
+++ test/data/constantpropagation/basic-loop-update-pre.js
@@ -0,0 +1,13 @@
+// Basic Loop with pre update expression
+/*
+var a = 0, b = 5;
+for (var i = 0; i < 5; ++i) {
+    a = a + 1;
+}
+var c = a + 5;
+*/
+var a = 0, b = 5;
+for(var i = 0; i < 5; ++i) {
+    a = a + 1;
+}
+var c = a + b;
