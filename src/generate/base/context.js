cb5ba9f863c0749b18395c4d9b1992b457ba611d
Kristian Sons <kristian.sons@dfki.de>
2013-12-18 12:05:43 +0100

    BaseContext: Functionality to add native functions to the context
    Sometime useful to use function to map functionality to.

--- src/generate/base/context.js
+++ src/generate/base/context.js
@@ -10,6 +10,13 @@
         this.usedParameters = {};
         this.variableNameMap = {};
         this.blockedNames = opt.blockedNames || [];
+
+       /**
+        * List of native functions that will be added during
+        * generation
+        * @type {Array.<string>}
+        */
+       this.nativeFunctions = [];
     };
     Tools.createClass(TransformContext, Context, {
         addUsedParameter: function (name, typeInfo) {
@@ -29,10 +36,17 @@
             return null;
         },
         setVariableName: function(oldName, newName) {
-            console.log("Set", oldName, newName);
-
             this.variableNameMap[oldName] = newName;
+        },
+        addNativeFunction: function(str) {
+            if(this.nativeFunctions.indexOf(str) == -1) {
+                this.nativeFunctions.push(str);
+            }
+        },
+        getNativeFunctions: function() {
+            return this.nativeFunctions;
         }
+
     });
 
     exports.TransformContext = TransformContext;
