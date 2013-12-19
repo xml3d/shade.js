2d73af2ef97b9b6c9d26dfdc884cd0b30dfc580b
Kristian Sons <kristian.sons@dfki.de>
2013-12-19 10:16:41 +0100

    OSL: Added more OSL keywords to compiler

--- src/generate/osl/compiler.js
+++ src/generate/osl/compiler.js
@@ -6,6 +6,11 @@
     var transform = require("./osl-transform.js").transform;
     var generate = require("./osl-generate.js").generate;
 
+    var OSLKeywords = ["time", "color", "break", "closure", "color", "continue", "do", "else", "emit", "float", "for", "if", "illuminance",
+                  "illuminate", "int", "matrix", "normal", "output", "point", "public", "return", "string", "struct", "vector", "void", "while"];
+
+    var OSLGlobals = ["P", "I", "N", "Ng", "dPdu", "dPdv", "u", "v", "time", "dtime", "dPdtime", "Ci"];
+
     var OSLCompiler = function (opt) {
         this.options = opt || {};
     };
@@ -15,7 +20,7 @@
         compile: function (aast) {
 
             var context = new OSLTransformContext(aast, "global.shade", Base.extend(this.options, {
-                blockedNames: ["N"]
+                blockedNames: OSLKeywords.concat(OSLGlobals)
             }));
 
             var transformed = transform(aast, context);
