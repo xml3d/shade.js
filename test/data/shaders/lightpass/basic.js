32d1dba0ec3e956bed394e6e1d1d16120a68ab00
Felix Klein <lachsen@cg.uni-saarland.de>
2013-12-20 12:22:25 +0100

    Fixed light pass tests.

--- test/data/shaders/lightpass/basic.js
+++ test/data/shaders/lightpass/basic.js
@@ -7,15 +7,16 @@
 // EXPECTED OUTPUT
 {
     function shade(env) {
-        var deferred0 = env.deferred0.sample2D(env.texcoord),
-            deferred1 = env.deferred1.sample2D(env.texcoord),
+        var texcoord = this.normalizedCoords.xy();
+        var deferred0 = env.deferred0.sample2D(texcoord),
+            deferred1 = env.deferred1.sample2D(texcoord),
             deferred2, deferred3, deferred4, deferred5, deferred6, deferred7;
         var ccId = deferred0.x();
         var position = deferred0.yzw();
         var ambientIntensity = deferred1.x();
         var ccArg0, ccArg1, ccArg2;
         if(ccId == 0){
-            deferred2 = env.deferred2.sample2D(env.texcoord);
+            deferred2 = env.deferred2.sample2D(texcoord);
             ccArg0 = deferred2.xyz();
             ccArg1 = deferred1.yzw();
             ccArg2 = deferred2.w();
