4f80580857c6f00cfa599c711e2e51eb5ab4f79e
Kristian Sons <kristian.sons@dfki.de>
2014-10-12 15:42:54 +0200

    Refactoring resolve.js

--- src/resolve/resolve.js
+++ src/resolve/resolve.js
@@ -1,43 +1,46 @@
-(function(ns){
+(function (ns) {
 
     var ColorClosureMarker = require("./colorclosure-marker.js");
 
-    var Implementations = {};
-    Implementations["xml3d-glsl-forward"] = require("./xml3d-glsl-forward/");
-    Implementations["xml3d-glsl-deferred"] = require("./xml3d-glsl-deferred/");
+    var c_implementations = {};
 
-    ns.resolveClosuresPreTypeInference = function(aast, implementationName, processingData, opt) {
-        if(!implementationName) {
+    var registerLightingImplementation = ns.registerLightingImplementation = function (name, obj) {
+        c_implementations[name] = obj;
+    };
+
+    ns.resolveClosuresPreTypeInference = function (aast, implementationName, processingData, opt) {
+        if (!implementationName) {
             return aast;
         }
         try {
-            var resolverImpl = Implementations[implementationName];
-            if(resolverImpl.resolvePreTypeInference){
+            var resolverImpl = c_implementations[implementationName];
+            if (resolverImpl.resolvePreTypeInference) {
                 ColorClosureMarker.markColorClosures(aast);
                 return resolverImpl.resolvePreTypeInference(aast, processingData, opt);
-            }
-            else
+            } else
                 return aast;
-        } catch(e) {
+        } catch (e) {
             console.error(e);
         }
         return aast;
-    }
+    };
 
-    ns.resolveClosuresPostTypeInference = function(aast, implementationName, processingData, opt) {
-        if(!implementationName) {
+    ns.resolveClosuresPostTypeInference = function (aast, implementationName, processingData, opt) {
+        if (!implementationName) {
             return aast;
         }
         try {
-           var resolverImpl = Implementations[implementationName];
-            if(resolverImpl.resolvePostTypeInference)
-                return resolverImpl.resolvePostTypeInference(aast, processingData, opt);
-            else
+            var resolverImpl = c_implementations[implementationName];
+            if (resolverImpl.resolvePostTypeInference)
+                return resolverImpl.resolvePostTypeInference(aast, processingData, opt); else
                 return aast;
-        } catch(e) {
+        } catch (e) {
             console.error(e);
         }
         return aast;
-    }
+    };
+
+    registerLightingImplementation("xml3d-glsl-forward", require("./xml3d-glsl-forward"));
+    registerLightingImplementation("xml3d-glsl-deferred", require("./xml3d-glsl-deferred"));
 
 }(exports));
