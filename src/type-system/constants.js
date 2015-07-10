a1ecc3c1c40ebc640aa1aa3b9e7bba4a591f84e5
Kristian Sons <kristian.sons@dfki.de>
2015-07-10 15:00:13 +0200

    Added missing constants.js

---
+++ src/type-system/constants.js
@@ -0,0 +1,16 @@
+/**
+ * @enum {string}
+ */
+module.exports.TYPES = {
+    ANY: "any",
+    INT: "int",
+    NUMBER: "number",
+    BOOLEAN: "boolean",
+    OBJECT: "object",
+    ARRAY: "array",
+    NULL: "null",
+    UNDEFINED: "undefined",
+    FUNCTION: "function",
+    STRING: "string",
+    INVALID: "invalid"
+};
